e0cfaea75ca042c6a3f408309898796a08f19a61
Avastbinder <ajvastbinder@gmail.com>
2025-02-14 15:35:44 -0700

    block fixed

--- Tests/test_aidan/Scenes/area_2d.gd
+++ Tests/test_aidan/Scenes/area_2d.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var here=false
+var entered = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,10 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if area_entered:
-		if here == false && Input.is_action_just_pressed("ui_accept"):
-			Dialogic.start("timeline")
-			here = true
-	elif area_exited:
-		Dialogic.end_timeline()
-		here = false
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+		if area_entered:
+			if Input.is_action_just_pressed("ui_accept"):
+				Dialogic.start("timeline")
+		elif area_exited:
+			Dialogic.end_timeline()
