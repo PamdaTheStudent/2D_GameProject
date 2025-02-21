245e0e3f9d8f2eba5353ea4af291fcf2707c9f06
yourusername <videogameacces@yahoo.com>
2025-02-21 16:25:40 -0700

    Fixed dialogue and removed tests
    Dialogue now works so player will stand still. This needs revision. Went ahead and deleted all tests in this new branch.

---
+++ mainproject/Scripts/npc_dialogue.gd
@@ -0,0 +1,25 @@
+class_name NPC
+
+extends Area2D
+
+var in_range = false
+
+signal Talking
+signal Free
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept") && in_range:
+			Talking.emit()
+			$CanvasLayer/DialogueBox.start()
+			in_range = false
+
+func _on_body_entered(body):
+	if body is player:
+		in_range = true
+
+func _on_body_exited(body):
+	if body is player:
+		in_range = false
+
+func _on_dialogue_box_dialogue_ended():
+	Free.emit()
