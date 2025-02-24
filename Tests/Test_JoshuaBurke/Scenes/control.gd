f24a073ed4fb56beb7b2171959ca58754721471c
Artemis-Fowl-2nd <145065140+Artemis-Fowl-2nd@users.noreply.github.com>
2025-02-24 14:40:51 -0700

    got saving done

---
+++ Tests/Test_JoshuaBurke/Scenes/control.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var database :SQLite
+var xSave : int
+var ySave : int
+var named : String
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	database = SQLite.new()
+	database.path = "res://boxes.db"
+	database.open_db()
+	var save_nodes = get_tree().get_nodes_in_group("savable")
+	for Node in save_nodes:
+		named = Node.name
+		xSave = Node.current_tile.x
+		
+		ySave = Node.current_tile.y
+		var data = {"Name":named,"Level":1,"x":xSave,"y":ySave}
+		database.insert_row("block",data)
+		
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+		
+			
+	pass
