6fd9a8530fe040b199c1934b92197a9fc74c9a80
yourusername <videogameacces@yahoo.com>
2025-03-19 16:48:26 -0600

    Finished level three

--- mainproject/Scenes/end.gd
+++ mainproject/Scenes/end.gd
@@ -4,12 +4,9 @@ extends Area2D
 
 var entered = 0
 
-func _on_body_entered(body: Node2D) -> void:
-	if entered == 0:
-		entered = 1
-	elif entered == 1:
-		entered = 2
-		animation_player.play("block_appear")
+func _on_body_entered(body: player) -> void:
+	animation_player.play("block_appear")
+	
 	
 
 
--- mainproject/Scenes/start_block.gd
+++ mainproject/Scenes/start_block.gd
@@ -5,8 +5,6 @@ var entered = 0
 func _ready() -> void:
 	collision_enabled = false
 
-func _on_end_body_entered(body: Node2D) -> void:
-	if entered == 0:
-		entered = 1
-	elif entered == 1:
-		collision_enabled = true
+func _on_end_body_entered(body: player) -> void:
+	collision_enabled = true
+	
