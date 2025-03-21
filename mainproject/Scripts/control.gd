9e6d2d58f5e6e931fb937af894715027f2445115
Artemis-Fowl-2nd <145065140+Artemis-Fowl-2nd@users.noreply.github.com>
2025-03-21 15:20:22 -0600

    Create control.gd
    the script?

---
+++ mainproject/Scripts/control.gd
@@ -0,0 +1,37 @@
+extends Control
+
+var database :SQLite
+var xSave : int
+var ySave : int
+var named : String
+Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    database = SQLite.new()
+    database.path = "res://boxes.db"
+    database.open_db()
+
+
+
+    pass # Replace with function body.
+
+
+Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+    if Input.is_action_pressed("1"):
+        var save_nodes = get_tree().get_nodes_in_group("save_node")
+        database.delete_rows("block","*")
+        for Node in save_nodes:
+            named = Node.name
+            if Node is player: 
+                xSave = Node.position.x
+                ySave = Node.position.y
+            elif Node is moveableBox:
+                print(named)
+                xSave = Node.current_tile.x
+                ySave = Node.current_tile.y
+            else: continue
+        var data = {"Name":named,"Level":1,"x":xSave,"y":ySave}
+        database.insert_row("block",data)
+
+    pass
