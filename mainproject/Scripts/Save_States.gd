d5e6b749c911244d3ff2869bfdb5d99c7ebd7594
Artemis-Fowl-2nd <145065140+Artemis-Fowl-2nd@users.noreply.github.com>
2025-03-26 16:35:20 -0600

    Create Save_States.gd

---
+++ mainproject/Scripts/Save_States.gd
@@ -0,0 +1,46 @@
+extends Control
+
+var database :SQLite
+var xSave : int
+var ySave : int
+var named : String
+var play : Array
+var boxes : Array
+#C alled when the node enters the scene tree for the first time.
+func _ready() -> void:
+	database = SQLite.new()
+	database.path = "res://boxes.db"
+	database.open_db()
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	if Input.is_action_just_pressed("save"):
+		var save_nodes = get_tree().get_nodes_in_group("save_node")
+		database.delete_rows("block","")
+		for Node in save_nodes:
+			named = Node.name
+			if Node is player: 
+				xSave = Node.position.x
+				ySave = Node.position.y
+			elif Node is moveableBox:
+				print(named)
+				xSave = Node.position.x
+				ySave = Node.position.y
+			else: continue
+		var data = {"Name":named,"Level":1,"x":xSave,"y":ySave}
+		database.insert_row("block",data)
+
+	if Input.is_action_just_pressed("load"):
+		play = database.select_rows("block","",["Name","x","y"])
+		for row in play:
+			var xload = row.get("x")
+			var yload = row.get("y")
+			var target = row.get("Name")
+			var node = get_tree().current_scene.find_child(target, true, false)
+			node.position = Vector2(xload, yload)
+			
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	pass
